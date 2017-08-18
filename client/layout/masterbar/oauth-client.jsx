1d99165a289010fc3892c18c6580f0b2c9f94e24
Jason Moon <jason.moon@automattic.com>
2017-08-18 14:11:47 -0600

    OAuth Login: Skip routing for logged out root '/‘

--- client/layout/masterbar/oauth-client.jsx
+++ client/layout/masterbar/oauth-client.jsx
@@ -5,6 +5,19 @@ import Gridicon from 'gridicons';
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Internal dependencies
+ */
+import { addLocaleToWpcomUrl, getLocaleSlug } from 'lib/i18n-utils';
+
+function returnToIndex( event ) {
+	// NOTE: This skips Calypso's routing logic and returns the logged out user
+	// back to WordPress.com homepage.
+	event.preventDefault();
+	const url = addLocaleToWpcomUrl( 'https://wordpress.com/', getLocaleSlug() );
+	window.location = url;
+}
+
 const OauthClientMasterbar = ( { oauth2ClientData } ) => (
 	<header className="masterbar masterbar__oauth-client">
 		<nav>
@@ -25,7 +38,11 @@ const OauthClientMasterbar = ( { oauth2ClientData } ) => (
 			) : (
 				<ul className="masterbar__oauth-client-user-nav">
 					<li className="masterbar__oauth-client-wpcc-sign-in">
-						<a href="https://wordpress.com/" className="masterbar__oauth-client-wpcom">
+						<a
+							href={ addLocaleToWpcomUrl( 'https://wordpress.com/', getLocaleSlug() ) }
+							className="masterbar__oauth-client-wpcom"
+							onClick={ returnToIndex }
+						>
 							<Gridicon icon="my-sites" size={ 24 } />
 							WordPress.com
 						</a>
